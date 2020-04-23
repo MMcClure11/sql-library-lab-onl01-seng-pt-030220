c729ac2f862361886cbdf080a6327e4d67aadda8
MMcClure11 <mcclme02@luther.edu>
2020-04-22 21:19:00 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,48 @@
+INSERT INTO series VALUES 
+  (1, 'Harry Potter', 1, 1), 
+  (2, 'Mistborn', 2, 2);
+
+INSERT INTO books VALUES 
+  (1, 'Sorcerers Stone', 1992, 1), 
+  (2, 'Chamber of Secrets', 1993, 1), 
+  (3, 'Prisoner', 1994, 1), 
+  (4, 'Goblet', 1994, 1),
+  (5, 'Order', 1995, 1),
+  (6, 'Half-blood', 1996, 1);
+
+INSERT INTO characters VALUES
+  (1, 'Harry', 'human', 'lightning bolt', 1),
+  (2, 'Hermione', 'human', 'smarty', 1),
+  (3, 'Hordax', 'centaur', 'big horsey', 1),
+  (4, 'miss piggy', 'pig', 'oink', 1),
+  (5, 'Harry', 'human', 'lightning bolt', 2),
+  (6, 'Hermione', 'human', 'smarty', 2),
+  (7, 'Hordax', 'centaur', 'big horsey', 2),
+  (8, 'miss piggy', 'pig', 'oink', 2);
+
+  INSERT INTO subgenres VALUES 
+    (1, 'fantasy'),
+    (2, 'high fantasy');
+
+
+INSERT INTO authors VALUES 
+  (1, 'J.K. Rowling'), 
+  (2, 'Brandon Sanderson');
+
+INSERT INTO character_books VALUES 
+  (1, 1, 1), 
+  (2, 1, 2), 
+  (3, 2, 2), 
+  (4, 3, 2), 
+  (5, 1, 3), 
+  (6, 2, 3), 
+  (7, 3, 3), 
+  (8, 1, 4),
+  (9, 4, 5), 
+  (10, 4, 6), 
+  (11, 5, 6), 
+  (12, 6, 6), 
+  (13, 4, 7), 
+  (14, 5, 7), 
+  (15, 6, 7), 
+  (16, 4, 8);
